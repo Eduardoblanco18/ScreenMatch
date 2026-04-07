8d9ba944fc989be00b76b4eb7aecc9025c0a67d3
Eduardo Domingues Blanco <eduardodominguesblanco@gmail.com>
2026-04-07 10:45:54 -0300

    Criando calculadora de tempo

--- src/Principal.java
+++ src/Principal.java
@@ -1,21 +1,22 @@
+import br.com.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.screenmatch.modelos.Filme;
 import br.com.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
-        Filme filme1 = new Filme();
-        filme1.setNome("O Poderoso Chefão");
-        filme1.setAnoDeLancamento(1972);
-        filme1.setDuracaoEmMinutos(175);
-        filme1.setIncluidoNoPlano(true);
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1972);
+        meuFilme.setDuracaoEmMinutos(175);
+        meuFilme.setIncluidoNoPlano(true);
 
-        filme1.exibeFichaTecnica();
-        filme1.avalia(8);
-        filme1.avalia(5);
-        filme1.avalia(10);
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
 
-        System.out.println("A média das avaliações é " + filme1.pegaMedia());
-        System.out.println("Total de avaliações: " + filme1.getTotalDeAvalicaoes());
+        System.out.println("A média das avaliações é " + meuFilme.pegaMedia());
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvalicaoes());
 
         Serie serie1 = new Serie();
         serie1.setNome("Lost");
@@ -25,5 +26,16 @@ public class Principal {
         serie1.setEpisodiosPorTemporada(15);
         serie1.setMinutosPorEpisodio(45);
         System.out.println("Duração para maratonar " + serie1.getNome() + ": " + serie1.getDuracaoEmMinutos());
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(serie1);
+        System.out.println(calculadora.getTempoTotal());
     }
 }
